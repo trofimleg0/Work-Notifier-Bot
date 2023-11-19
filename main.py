3561058960d619c05962436e0be39bcdb1fbbdf7
Oleg Trofimov <trofimlego@MacBook-Air-Oleg.local>
2023-11-19 22:33:18 +0100

    Updated script and requirements.txt

--- main.py
+++ main.py
@@ -4,39 +4,44 @@ import requests
 import telegram
 from environs import Env
 
-env = Env()
-env.read_env()  # read .env file, if it exists
 
-auth_token = env.str("AUTHORIZATION_TOKEN")
-telegram_chat_id = env.str("TELEGRAM_CHAT_ID")
+def main():
+    env = Env()
+    env.read_env()  # read .env file, if it exists
 
-bot = telegram.Bot(token=env.str("TELEGRAM_TOKEN"))
+    auth_token = env.str("AUTHORIZATION_TOKEN")
+    telegram_chat_id = env.str("TELEGRAM_CHAT_ID")
 
-url = "https://dvmn.org/api/long_polling/"
-headers = {"Authorization": f"Token {auth_token}"}
-params = {"timestamp": time()}
+    bot = telegram.Bot(token=env.str("TELEGRAM_TOKEN"))
 
-while True:
-    try:
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-        json_respone_data = response.json()
+    url = "https://dvmn.org/api/long_polling/"
+    headers = {"Authorization": f"Token {auth_token}"}
+    params = {"timestamp": time()}
 
-        if json_respone_data["status"] == "found":
-            new_attempts = json_respone_data["new_attempts"][0]
-            text = f'You have checked the work of "{new_attempts["lesson_title"]}"!\n\n'
-            if new_attempts["is_negative"]:
-                text += "Unfortunately, there were errors in the work!\n"
-            else:
-                text += "The teacher liked everything, you can proceed to the next lesson!\n"
-            text += f'Go to the lesson to see {new_attempts["lesson_url"]}'
-            bot.send_message(chat_id=telegram_chat_id, text=text)
-        elif json_respone_data["status"] == "timeout":
-            params = {"timestamp": json_respone_data["timestamp_to_request"]}
+    while True:
+        try:
+            response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()
+            checking_result = response.json()
 
-    except requests.exceptions.ReadTimeout:
-        print("Time occurred!")
-    except requests.exceptions.ConnectionError:
-        sleep(5)
-        print("No internet connection!")
-        continue
+            if checking_result["status"] == "found":
+                new_attempts = checking_result["new_attempts"][0]
+                text = f'You have checked the work of "{new_attempts["lesson_title"]}"!\n\n'
+                if new_attempts["is_negative"]:
+                    text += "Unfortunately, there were errors in the work!\n"
+                else:
+                    text += "The teacher liked everything, you can proceed to the next lesson!\n"
+                text += f'Go to the lesson to see {new_attempts["lesson_url"]}'
+                bot.send_message(chat_id=telegram_chat_id, text=text)
+            elif checking_result["status"] == "timeout":
+                params = {"timestamp": checking_result["timestamp_to_request"]}
+
+        except requests.exceptions.HTTPError:
+            continue
+        except requests.exceptions.ConnectionError:
+            sleep(5)
+            continue
+
+
+if __name__ == "__main__":
+    main()
